42a013f5d699d7709308a0a2ab9fa118b93b2128
ckmagana <ckmagana07@gmail.com>
2021-05-20 21:05:16 +0800

    frontend-contact

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,6 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 Route::view('/about-us', 'about-us')->name('about-us');
+Route::view('/contact', 'contact')->name('contact');
 
 require __DIR__.'/auth.php';
