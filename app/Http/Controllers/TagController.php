e4b22c2e1603ae91a5828119b0760e5281fdd27e
Philiks <fjanopol@gmail.com>
2021-06-02 10:37:31 +0800

    Removed unnecessary methods (only create left)

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -9,16 +9,6 @@ use Illuminate\Http\Request;
 class TagController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Database\Eloquent\Collection|static[]
-     */
-    public function index()
-    {
-        return Tag::all();
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -33,29 +23,5 @@ class TagController extends Controller
         $tag = Tag::create(['title' => $request->title]);
         
         return $tag;
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Tag  $tag
-     * @return \App\Models\Tag  $tag
-     */
-    public function show(Tag $tag)
-    {
-        return $tag;
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Tag  $tag
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Tag $tag)
-    {
-        $tag->delete();
-
-        return back();
     }
 }
