62771d02c4d1f869ecee4f63876a3c8d7143f650
Philiks <fjanopol@gmail.com>
2021-06-05 22:06:59 +0800

    Added registration testing with UploadImage

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -4,6 +4,8 @@ namespace Tests\Feature;
 
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
 class RegistrationTest extends TestCase
@@ -25,6 +27,21 @@ class RegistrationTest extends TestCase
             'email' => 'test@example.com',
             'password' => 'password',
             'password_confirmation' => 'password',
+        ]);
+
+        $this->assertAuthenticated();
+        $response->assertRedirect(RouteServiceProvider::HOME);
+    }
+
+    public function test_new_users_can_register_with_photo()
+    {
+        $response = $this->post('/register', [
+            'fullname' => 'Test User',
+            'username' => 'test_user',
+            'email' => 'test@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+            'profile_photo' => UploadedFile::fake()->image('avatar.jpg'),
         ]);
 
         $this->assertAuthenticated();
