c835316f3d36b108c195f40dc56a958c6bf66bd3
Philiks <fjanopol@gmail.com>
2021-06-05 21:39:36 +0800

    Resizes the photo before storing

--- app/Services/PhotoService.php
+++ app/Services/PhotoService.php
@@ -6,10 +6,16 @@ use App\Models\Blog;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\UploadedFile;
+use Intervention\Image\ImageManagerStatic as Image;
 use InvalidArgumentException;
 
 class PhotoService
 {
+    const PROFILE_WIDTH = 240;
+    const PROFILE_HEIGHT = 240;
+    const BLOG_WIDTH = 360;
+    const BLOG_HEIGHT = 300;
+
     /**
      * Store the uploaded photo on the filesystem disk.
      * 
@@ -27,15 +33,22 @@ class PhotoService
 
         if (!($model instanceof User || $model instanceof Blog))
             throw new InvalidArgumentException('$model is not an instance of User or Blog');
+        
+        $field = '';
+        if ($model instanceof User) {
+            $field = 'profile_photo';
+            $width = PhotoService::PROFILE_WIDTH;
+            $height = PhotoService::PROFILE_HEIGHT;
+        } else if ($model instanceof Blog) {
+            $field = 'main_photo';
+            $width = PhotoService::BLOG_WIDTH;
+            $height = PhotoService::BLOG_HEIGHT;
+        }
 
         $filename = $model->id . '/' . $photo->getClientOriginalName();
-        $photo->storeAs($folder_name, $filename);
-
-        $field = '';
-        if ($model instanceof User)
-            $field = 'profile_photo';
-        else if ($model instanceof Blog)
-            $field = 'main_photo';
+        Image::make($photo->getRealPath())
+            ->resize($width, $height)
+            ->save(storage_path($folder_name, $filename));
 
         $model->update([$field => $folder_name . '/' . $filename]);
     }
