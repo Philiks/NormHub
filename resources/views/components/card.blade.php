d767f0d18d36f8dfc9c01a740e6099efa4b99b98
Philiks <fjanopol@gmail.com>
2021-06-05 21:45:43 +0800

    Created card blade without logo

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,5 @@
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-black">
+    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-gradient-to-br from-orange shadow-md overflow-hidden sm:rounded-lg">
+        {{ $slot }}
+    </div>
+</div>
