7a04184059a93086a13bfe924a8588678c7032da
Philiks <fjanopol@gmail.com>
2021-06-06 11:54:10 +0800

    Livewire for Displaying Profile Photo

---
+++ app/Http/Livewire/DisplayProfile.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class DisplayProfile extends Component
+{
+    public $profile;
+
+    public function mount()
+    {
+        if (auth()->user()->profile_photo != null)
+            $this->profile = asset('storage/' . auth()->user()->profile_photo);
+    }
+
+    public function render()
+    {
+        return view('livewire.display-profile');
+    }
+}
