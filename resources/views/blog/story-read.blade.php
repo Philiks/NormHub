cf0e4ab9db1cc4bf6154b74e31b41a4bc0c1a0b3
Philiks <fjanopol@gmail.com>
2021-06-08 00:08:17 +0800

    Story Views

---
+++ resources/views/blog/story-read.blade.php
@@ -0,0 +1,44 @@
+<x-app-layout>
+    <x-wide-card>
+        <div class="flex flex-col items-center">
+            <!-- Title -->
+            <div class="p-3 font-bold text-orange text-center text-xl lg:text-3xl text-white">
+                {{ __($blog->title) }}
+            </div>
+            
+            <!-- Photo -->
+            <img class="mt-4 self-center rounded-2xl bg-cover" 
+                        src="{{ $blog->main_photo != null ?
+                            asset('images/' . $blog->main_photo) :
+                            asset('storage/defaults/blog.png') }}" 
+                        width="420" height="350" />
+    
+            <div class="mt-4 w-1/2 bg-gradient-to-l from-orange rounded-lg p-3 text-sm text-white lg:text-base">
+                <div class="flex items-center">
+                    <!-- Author Profile Photo -->
+                    @livewire('display-profile', ['user' => $blog->author])
+
+                    <div>
+                        <div class="text-base lg:text-lg">
+                            <!-- Author -->
+                            {{ $blog->author->username }}
+                        </div>
+
+                        <div class="text-sm lg:text-base">
+                            <!-- Date Created and Read Time -->
+                            {!! $blog->created_at->format('M j') . ' &bull; ' . $blog->read_time . ' min read'!!}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <!-- Content -->
+            <div class="w-full mt-4 bg-orange-dark rounded-lg p-3 text-sm text-white lg:text-base">
+                {!! $blog->format_content() !!}
+            </div>
+    
+            <!-- Tags -->
+            @livewire('display-tags', ['tags' => $blog->tags])
+        </div>
+    </x-wide-card>
+</x-app-layout>
---
+++ resources/views/blog/story-write.blade.php
@@ -0,0 +1,40 @@
+<x-app-layout>
+    <x-wide-card>
+        <!-- Validation Errors -->
+        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+        
+        <form method="POST" action="{{ route('blog.store') }}" enctype="multipart/form-data">
+            @csrf
+
+            <!-- Title -->
+            <div>
+                <x-label for="title" :value="__('Title')" />
+
+                <x-input id="title" class="text-center text-xl lg:text-3xl block mt-1 w-full" type="text" name="title" :value="old('title')" required autofocus />
+            </div>
+
+            <!-- Photo -->
+            @livewire('upload-image', ['default_photo' => asset('storage/defaults/blog.png'), 'is_round' => false])
+
+            <!-- Content -->
+            <div>
+                <x-label for="content" :value="__('Content')" />
+
+                <x-textarea id="content" class="block mt-1 w-full" type="text" name="content" :value="old('content')" required />
+            </div>
+
+            <!-- Tags -->
+            <div class="mt-4">
+                <x-label for="tags" :value="__('Tags *separate multiple tags using comma \',\' i.e. abc,def.')" />
+
+                <x-input id="tags" class="block mt-1 w-full" type="text" name="tags" :value="old('tags')" required />
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <x-button class="ml-4">
+                    {{ __('Publish') }}
+                </x-button>
+            </div>
+        </form>
+    </x-wide-card>
+</x-app-layout>
