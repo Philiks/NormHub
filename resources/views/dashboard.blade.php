bda820f27ffc85619519e613b90f2f4ed50717da
Philiks <fjanopol@gmail.com>
2021-06-08 00:07:48 +0800

    Used livewire component

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,7 @@
 <x-app-layout>
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
-                </div>
-            </div>
+        <div class="grid grid-cols-3 gap-5 px-5">
+            @livewire('display-featured-blogs')
         </div>
     </div>
 </x-app-layout>
