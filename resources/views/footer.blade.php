12975657cfc71100a51ae01145ee739371fc52f6
Philiks <fjanopol@gmail.com>
2021-06-10 20:12:01 +0800

    Re designed footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,15 +1,8 @@
-<x-app-layout>
-    <footer>
-        <div class="container lg mx-24 bg-gradient-to-r from-orange to-black px-12 border-gray-50 fixed">
-            <div class="bg-gradient-to-r from-orange to-black">
-            <h3 class="text-white"><strong><center>For more information visit and contact us in:</center></strong></h3>
-            <center>
-            <a href="#"><img src="...." alt="Avatar" class="avatar" style="width: 50px; height: 50px; border-radius: 50%;"></a>
-            <a href="#"><img src="...." alt="Avatar" class="avatar" style="width: 50px; height: 50px; border-radius: 50%;"></a>
-            <a href="#"><img src="...." alt="Avatar" class="avatar" style="width: 50px; height: 50px; border-radius: 50%;"></a>
-            <p class="text-white">Copyright © 2021 BSCS-3A </p>
-            </center>
-            </div>
-        </div>
-    </footer>
-</x-app-layout>
+<footer class="w-full py-5">
+    <div class="w-full px-12 border-gray-50">
+        <h3 class="text-orange"><strong><center>For more information visit and contact us in:</center></strong></h3>
+        <center>
+            <p class="text-orange">Copyright © 2021 BSCS-3A </p>
+        </center>
+    </div>
+</footer>
