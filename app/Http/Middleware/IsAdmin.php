471f380ba8a87eb2a29238592f266eddf76b484e
Philiks <fjanopol@gmail.com>
2021-06-10 20:09:37 +0800

    is Admin Middleware

---
+++ app/Http/Middleware/IsAdmin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class IsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (auth()->user() && auth()->user()->is_admin)
+            return $next($request);
+
+        return redirect('dashboard')->with('error', 'You do not have admin access');
+    }
+}
